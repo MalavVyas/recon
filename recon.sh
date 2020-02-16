d7d92a3582d2e1f5276409a8959cf8a7dbea583c
Malav Vyas <malavvyas98@gmail.com>
2020-02-16 10:57:47 +0530

    Create recon.sh

---
+++ recon.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+mkdir nullrecon
+cd nullrecon
+echo "[+] Installing googled0rker"
+mkdir googd0rker
+cd googd0rker
+pip install google --user
+wget https://raw.githubusercontent.com/ZephrFish/GoogD0rker/master/googd0rker.py
+cd ..
+echo "[+] Installing assetfinder"
+mkdir assetfinder
+cd assetfinder
+go get -u github.com/tomnomnom/assetfinder
+cd ..
+echo "[+] installing gf"
+mkdir gf
+go get -u github.com/tomnomnom/gf
+echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
+source ~/.bashrc
+cd ..
+echo "[+] Instaiing meg"
+mkdir meg
+cd meg
+go get -u github.com/tomnomnom/meg
+cd ..
+echo "[+]Installing ffuf"
+mkdir ffuf
+cd ffuf
+go get github.com/ffuf/ffuf
+cd ..
+echo "[+] Installing gitrib"
+mkdir gitrob
+cd gitrob
+go get github.com/michenriksen/gitrob
+cd ..
+echo "[+] Installing trufflehog"
+mkdir trufflehog
+cd trufflehog
+pip install truffleHog --user
+cd ..
+echo "[+] Installing linkfinder"
+mkdir linkfinder
+cd linkfinder
+git clone https://github.com/GerbenJavado/LinkFinder.git
+cd LinkFinder
+python setup.py install
+pip3 install -r requirements.txt
+cd ..
+cd ..
+echo "[+] Installing goohak"
+mkdir goohak
+cd goohak
+wget https://raw.githubusercontent.com/1N3/Goohak/master/goohak
+echo 'alias trufflehog="/root/.local/bin/trufflehog' >> ~/.bashrc
+source ~/.bashrc
+echo "[+] Done!"
